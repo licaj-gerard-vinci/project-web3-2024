6f526f88b1f54fd23f26570decf80492ef2c0e3e
Gerard Licaj <gerard@MacBook-Pro-de-Gerard.local>
2024-11-05 12:43:00 +0100

    fixed body display

--- project-web3-2024/src/pages/Muscle/BodyMap.js
+++ project-web3-2024/src/pages/Muscle/BodyMap.js
@@ -5,23 +5,59 @@ import { ReactComponent as BodyBack } from '../../assets/bodyBack.svg';
 
 const BodyMap = () => {
   const [selectedMuscle, setSelectedMuscle] = useState(null);
+  const [isFrontView, setIsFrontView] = useState(true);
 
   const handleMuscleClick = (e) => {
     const muscle = e.target.id;
     setSelectedMuscle(muscle);
   };
 
+  const toggleView = () => {
+    setIsFrontView((prevView) => !prevView);
+  };
+
   return (
-    <div className="body-map">
-      <BodyFront onClick={handleMuscleClick} className="svg-body" />
-      <BodyBack onClick={handleMuscleClick} className="svg-body" />
-      
-      {selectedMuscle && (
-        <div className="muscle-info">
-          <h2>{selectedMuscle}</h2>
-          <p>Informations sur le muscle {selectedMuscle}...</p>
+    <div className="main-container">
+      {/* Left Column: Body Map */}
+      <div className="body-map-container">
+        {/* SVG for Front View */}
+        <div className={`svg-body front-view ${isFrontView ? '' : 'hide-front'}`}>
+          <BodyFront onClick={handleMuscleClick} />
         </div>
-      )}
+
+        {/* SVG for Back View */}
+        <div className={`svg-body back-view ${!isFrontView ? 'show-back' : ''}`}>
+          <BodyBack onClick={handleMuscleClick} />
+        </div>
+
+        {/* Toggle Button below body map */}
+        <button className="toggle-button" onClick={toggleView}>
+          {isFrontView ? 'Switch to Back View' : 'Switch to Front View'}
+        </button>
+      </div>
+
+      {/* Right Column: Exercise List and Videos */}
+      <div className="exercise-list-container">
+        {selectedMuscle ? (
+          <>
+            <h2>{selectedMuscle}</h2>
+            <p>List of exercises for {selectedMuscle}:</p>
+            <ul>
+              <li>Exercise 1</li>
+              <li>Exercise 2</li>
+              <li>Exercise 3</li>
+              <li>More exercises...</li>
+            </ul>
+            <p>Video tutorials:</p>
+            <ul>
+              <li>Video 1</li>
+              <li>Video 2</li>
+            </ul>
+          </>
+        ) : (
+          <p>Select a muscle to see exercises and videos.</p>
+        )}
+      </div>
     </div>
   );
 };
