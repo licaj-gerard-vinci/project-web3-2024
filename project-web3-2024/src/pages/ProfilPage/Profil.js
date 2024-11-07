1b2f4d94ae9f1879c64c97c605cba95b359662df
Gerard <gerard.licaj@student.vinci.be>
2024-11-07 23:24:51 +0100

    added form

--- project-web3-2024/src/pages/ProfilPage/Profil.js
+++ project-web3-2024/src/pages/ProfilPage/Profil.js
@@ -1,21 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import React, { useState, useEffect, useRef } from 'react';
-import { ref, get, update } from 'firebase/database';
-import { db, storage } from '../../firebaseConfig';
+import { ref, get } from 'firebase/database';
+import { db } from '../../firebaseConfig';
 import { useNavigate } from 'react-router-dom';
-import { getDownloadURL, uploadBytes, ref as storageRef } from 'firebase/storage';
+import ReactModal from 'react-modal';
+import ProfileForm from '../Survery/ProfileForm';
 import './Profil.css';
+
+ReactModal.setAppElement('#root');
 
 const Profil = () => {
   const [user, setUser] = useState(null);
-  const [prenom, setPrenom] = useState('');
-  const [nom, setNom] = useState('');
-  const [age, setAge] = useState('');
-  const [gender, setGender] = useState('');
-  const [photoURL, setPhotoURL] = useState('');
-  const [favorites, setFavorites] = useState([]);
-  const [isEditing, setIsEditing] = useState(false);
-  const fileInputRef = useRef(null);
+  const [profileData, setProfileData] = useState({});
+  const [showFormModal, setShowFormModal] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -24,161 +21,57 @@ const Profil = () => {
       if (currentUser) {
         setUser(currentUser);
         await fetchUserProfile(currentUser.uid);
-        await fetchFavorites(currentUser.uid);
       } else {
         navigate('/login');
       }
     });
-
     return () => unsubscribe();
   }, [navigate]);
 
-  // Fonction pour récupérer le profil de l'utilisateur
   const fetchUserProfile = async (userId) => {
     const userRef = ref(db, `users/${userId}`);
     const snapshot = await get(userRef);
     const userData = snapshot.val();
-
-    setPrenom(userData?.prenom || '');
-    setNom(userData?.nom || '');
-    setAge(userData?.age || '');
-    setGender(userData?.gender || '');
-    setPhotoURL(userData?.photoURL || '');
+    setProfileData(userData || {});
   };
 
-  // Fonction pour récupérer les exercices favoris de l'utilisateur
-  const fetchFavorites = async (userId) => {
-    const userRef = ref(db, `users/${userId}/favorites`);
-    const snapshot = await get(userRef);
-
-    if (snapshot.exists()) {
-      const favoriteIds = snapshot.val();
-      const exercisePromises = favoriteIds.map((exerciseId) =>
-        get(ref(db, `exercises/${exerciseId}`))
-      );
-
-      const exerciseSnapshots = await Promise.all(exercisePromises);
-      const favoriteExercises = exerciseSnapshots
-        .filter((snap) => snap.exists())
-        .map((snap) => snap.val().name);
-
-      setFavorites(favoriteExercises);
-    }
-  };
-
-  const handlePhotoChange = async (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const photoStorageRef = storageRef(storage, `users/${user.uid}/pp`);
-      await uploadBytes(photoStorageRef, file);
-      const downloadURL = await getDownloadURL(photoStorageRef);
-      const userRef = ref(db, `users/${user.uid}`);
-      await update(userRef, { photoURL: downloadURL });
-      setPhotoURL(downloadURL);
-    }
-  };
-
-  const triggerFileInput = () => {
-    fileInputRef.current.click();
-  };
-
-  // Gérer l'édition des informations
-  const toggleEdit = () => {
-    if (isEditing) {
-      saveChanges();
-    }
-    setIsEditing(!isEditing);
-  };
-
-  // Enregistrer les modifications dans la DB
-  const saveChanges = async () => {
-    const userRef = ref(db, `users/${user.uid}`);
-    await update(userRef, {
-      prenom,
-      nom,
-      age,
-      gender
-    });
-    // Recharger les données après la sauvegarde
-    await fetchUserProfile(user.uid);
-  };
+    // Toggle the "no-scroll" class on the body element when the modal opens/closes
+    useEffect(() => {
+      if (showFormModal) {
+        document.body.classList.add('no-scroll');
+      } else {
+        document.body.classList.remove('no-scroll');
+      }
+      // Clean up the class when the component is unmounted
+      return () => document.body.classList.remove('no-scroll');
+    }, [showFormModal]);
+  
 
   return (
     <div className="profile-container">
       <div className="profile-info">
-        <div className="profile-photo-container" onClick={triggerFileInput}>
-          <img
-            src={photoURL || "https://via.placeholder.com/100"}
-            alt="Photo de profil"
-            className="profile-photo"
-          />
-          <input
-            type="file"
-            ref={fileInputRef}
-            style={{ display: 'none' }}
-            onChange={handlePhotoChange}
-          />
-        </div>
         <h1>Votre Profil</h1>
-        
-        <div className="form-group">
-          <label>Prénom :</label>
-          {isEditing ? (
-            <input type="text" value={prenom} onChange={(e) => setPrenom(e.target.value)} />
-          ) : (
-            <p>{prenom}</p>
-          )}
-        </div>
-        
-        <div className="form-group">
-          <label>Nom :</label>
-          {isEditing ? (
-            <input type="text" value={nom} onChange={(e) => setNom(e.target.value)} />
-          ) : (
-            <p>{nom}</p>
-          )}
-        </div>
-        
-        <div className="form-group">
-          <label>Âge :</label>
-          {isEditing ? (
-            <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
-          ) : (
-            <p>{age}</p>
-          )}
-        </div>
-        
-        <div className="form-group">
-          <label>Sexe :</label>
-          {isEditing ? (
-            <select value={gender} onChange={(e) => setGender(e.target.value)}>
-              <option value="">Sélectionner</option>
-              <option value="male">Homme</option>
-              <option value="female">Femme</option>
-            </select>
-          ) : (
-            <p>{gender}</p>
-          )}
-        </div>
+        <p>{profileData?.prenom}</p>
+        <p>{profileData?.nom}</p>
+        <p>{profileData?.age}</p>
+        <p>{profileData?.gender}</p>
 
-        <button className="edit-button" onClick={toggleEdit}>
-          {isEditing ? "Enregistrer" : "Modifier information"}
+        <button className="complete-profile-button" onClick={() => setShowFormModal(true)}>
+          Complete Profile
         </button>
 
-        <h2>Exercices Favoris</h2>
-        <ul>
-          {favorites.length > 0 ? (
-            favorites.map((exercise, index) => (
-              <li key={index}>{exercise}</li>
-            ))
-          ) : (
-            <p>Aucun exercice favori trouvé.</p>
-          )}
-        </ul>
-
-        
-        
-        <button className="logout-button" onClick={() => getAuth().signOut()}>Déconnexion</button>
+        <ReactModal
+          isOpen={showFormModal}
+          onRequestClose={() => setShowFormModal(false)}
+          className="modal-content"
+          overlayClassName="modal-overlay"
+          closeTimeoutMS={300}
+        >
+          <ProfileForm
+            onClose={() => setShowFormModal(false)}
+            initialData={{ prenom: profileData.prenom, nom: profileData.nom }}
+          />
+        </ReactModal>
       </div>
     </div>
   );
