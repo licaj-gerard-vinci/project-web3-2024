25934957c5d113dfc9d18162e6383dd70e484266
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2024-11-11 21:48:31 +0100

    ff

--- project-web3-2024/src/pages/NavBar/NavBar.js
+++ project-web3-2024/src/pages/NavBar/NavBar.js
@@ -1,12 +1,28 @@
-// NavBar.js
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
-import ImageDisplay from '../../components/Image/ImageDisplay';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import { getDownloadURL, ref as storageRef } from 'firebase/storage';
+import { storage } from '../../firebaseConfig';
 import './Navbar.css';
+import ImageDisplay from '../../components/Image/ImageDisplay';
 
 function Navbar({ user, handleLogout }) {
   const navigate = useNavigate();
   const location = useLocation();
+  const [profileImage, setProfileImage] = useState(null);
+
+  useEffect(() => {
+    const loadProfileImage = async () => {
+      if (user && user.photoURL) {
+        setProfileImage(user.photoURL);
+      } else {
+        const defaultImageRef = storageRef(storage, 'HomePage/NavBar/default-pp.png');
+        const url = await getDownloadURL(defaultImageRef);
+        setProfileImage(url);
+      }
+    };
+    loadProfileImage();
+  }, [user]);
 
   const handleLogoutClick = () => {
     handleLogout();
@@ -16,30 +32,72 @@ function Navbar({ user, handleLogout }) {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <ImageDisplay imagePath="HomePage/logoM.png" altText="Logo du site" />
+        <Link to="/">
+          <ImageDisplay imagePath="HomePage/logoM.png" />
+        </Link>
       </div>
-      <ul className="navbar-links">
-        <li>
-          <Link to="/" className={`navbar-item ${location.pathname === '/' ? 'active' : ''}`}>Home</Link>
-        </li>
-        <li>
-          <Link to="/bodyMap" className={`navbar-item ${location.pathname === '/bodyMap' ? 'active' : ''}`}>BodyMap</Link>
-        </li>
-        {!user ? (
+      
+      {/* Section centrée pour les liens Home et BodyMap */}
+      <div className="navbar-center-links">
+        <ul>
           <li>
-            <Link to="/authForm" className={`navbar-item ${location.pathname === '/authForm' ? 'active' : ''}`}>Sign in</Link>
+            <Link to="/" className={`navbar-item ${location.pathname === '/' ? 'active' : ''}`}>
+              Home
+            </Link>
           </li>
-        ) : (
-          <>
+          <li>
+            <Link to="/bodyMap" className={`navbar-item ${location.pathname === '/bodyMap' ? 'active' : ''}`}>
+              BodyMap
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Section à droite pour les éléments conditionnels */}
+      <div className="navbar-right-links">
+        <ul>
+          {user ? (
             <li>
-              <Link to="/profil" className={`navbar-item ${location.pathname === '/profil' ? 'active' : ''}`}>Profil</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger asChild>
+                  <div className="profile-photo-container">
+                    <img
+                      src={profileImage || '/path/to/default-image.png'}
+                      alt="Profile"
+                      className="profile-photo"
+                    />
+                  </div>
+                </DropdownMenu.Trigger>
+
+                <DropdownMenu.Content
+                  className="dropdown-content"
+                  align="end"
+                  sideOffset={5}
+                >
+                  <DropdownMenu.Item
+                    className="dropdown-item"
+                    onClick={() => navigate('/profil')}
+                  >
+                    Profil
+                  </DropdownMenu.Item>
+                  <DropdownMenu.Item
+                    className="dropdown-item"
+                    onClick={handleLogoutClick}
+                  >
+                    Déconnexion
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             </li>
+          ) : (
             <li>
-              <button onClick={handleLogoutClick} className={`navbar-item logout-button ${location.pathname === '/logout' ? 'active' : ''}`}>Sign out</button>
+              <Link to="/authForm" className={`navbar-item ${location.pathname === '/authForm' ? 'active' : ''}`}>
+                Sign in
+              </Link>
             </li>
-          </>
-        )}
-      </ul>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 }
