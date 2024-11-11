85e2e70f5f2fae7e9e38dbe81faa29ef0a62d7db
Henry Obedekah <henry.obedekah@student.be>
2024-11-11 12:27:22 +0100

    Calculteur de BMI

--- project-web3-2024/src/pages/HomePage/Divider/Divider.js
+++ project-web3-2024/src/pages/HomePage/Divider/Divider.js
@@ -1,68 +1,45 @@
 import React from "react";
 import "./Divider.css";
 
-const Divider = () => {
-    return(
+const Divider = ({ type = "line", text }) => {
+    return (
         <>
-    <div className="divider"></div>   
-        <div className="scrolling-line">
-            <div class="scrolling-line">
-                <div class="scrolling-content">
-                    <span>STAY HUMBLE</span>
-                    <span>✦</span>
-                    <span> BE PROUD </span>
-                    <span>✦</span>
-                    <span> BELIEVE IN YOURSELF</span>
-                    <span>✦</span>
-                    <span> NEVER GIVE UP</span>
-                    <span>✦</span>
-                    <span> TRAIN HARD </span>
-                    <span>✦</span>
-                    <span> NO PAIN, NO GAIN </span>
-                    <span>✦</span>
-                    <span> BE STRONG </span>
-                    <span>✦</span>
-                    <span> BE YOURSELF</span>
-                    <span>✦</span>
-                    <span> FEEL THE BURN </span>
-                    <span>✦</span>
-                    <span> BE A WARRIOR</span>
-                    <span>✦</span>
-                    <span> FOCUS ON THE GOAL</span>
-                    <span>✦</span>
-                    <span> STAY DISCIPLINED  </span>
-                    <span>✦</span>
-                    <span>STAY HUMBLE</span>
-                    <span>✦</span>
-                    <span> BE PROUD </span>
-                    <span>✦</span>
-                    <span> BELIEVE IN YOURSELF</span>
-                    <span>✦</span>
-                    <span> NEVER GIVE UP</span>
-                    <span>✦</span>
-                    <span> TRAIN HARD </span>
-                    <span>✦</span>
-                    <span> NO PAIN, NO GAIN </span>
-                    <span>✦</span>
-                    <span> BE STRONG </span>
-                    <span>✦</span>
-                    <span> BE YOURSELF</span>
-                    <span>✦</span>
-                    <span> FEEL THE BURN </span>
-                    <span>✦</span>
-                    <span> BE A WARRIOR</span>
-                    <span>✦</span>
-                    <span> FOCUS ON THE GOAL</span>
-                    <span>✦</span>
-                    <span> STAY DISCIPLINED  </span>
-                    <span>✦</span>
+            {type === "line" && (
+                <div className="divider-line">
+                    <span className="divider-text">{text}</span>
                 </div>
-            </div>
-        </div>
-    </>
-
-
-    )
+            )}
+            {type === "scrolling" && (
+                <div className="scrolling-line">
+                    <div className="scrolling-content">
+                        <span>STAY HUMBLE</span>
+                        <span>✦</span>
+                        <span> BE PROUD </span>
+                        <span>✦</span>
+                        <span> BELIEVE IN YOURSELF</span>
+                        <span>✦</span>
+                        <span> NEVER GIVE UP</span>
+                        <span>✦</span>
+                        <span> TRAIN HARD </span>
+                        <span>✦</span>
+                        <span> NO PAIN, NO GAIN </span>
+                        <span>✦</span>
+                        <span> BE STRONG </span>
+                        <span>✦</span>
+                        <span> BE YOURSELF</span>
+                        <span>✦</span>
+                        <span> FEEL THE BURN </span>
+                        <span>✦</span>
+                        <span> BE A WARRIOR</span>
+                        <span>✦</span>
+                        <span> FOCUS ON THE GOAL</span>
+                        <span>✦</span>
+                        <span> STAY DISCIPLINED</span>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 };
 
 export default Divider;
